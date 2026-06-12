59da9f2a3a667fee03f9bc641abf7b3d7e8e06d5
Eric <eric.schubert.3d@gmail.com>
2026-06-12 09:59:12 +0200

    Reduced bubble sort complexity

--- Sorting_Algorithms/01_bubblesort.py
+++ Sorting_Algorithms/01_bubblesort.py
@@ -1,11 +1,12 @@
 values = [30,24,12,50,11,49,11,5,2,5,7,55,100,0,7,2,1]
 
+
 def bubblesort(values):
-    for i in range(len(values) - 2):
-        for j in range(len(values) -1):
+    for i in range(len(values) - 1):
+        for j in range(len(values) -1 - i):
             if values[j] > values[j+1]:
-                temp = values[j]
                 values[j], values[j+1] = values[j+1], values[j]
+                print(values)
     return values
 
 print(bubblesort(values))
